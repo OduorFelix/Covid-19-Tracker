9033b9338a4a9b5a6d9040536e0e3129e288d830
ODUOR FELIX <foduor@ngamia.africa>
2020-08-26 17:27:35 +0300

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAfxdFAGE89pQo7wEqu4gIIcr3rfMZGtqA",
+    authDomain: "covid-19-tracker-6b6c5.firebaseapp.com",
+    databaseURL: "https://covid-19-tracker-6b6c5.firebaseio.com",
+    projectId: "covid-19-tracker-6b6c5",
+    storageBucket: "covid-19-tracker-6b6c5.appspot.com",
+    messagingSenderId: "626743868440",
+    appId: "1:626743868440:web:3d56bf5224caa7138c176d",
+    measurementId: "G-CS8Z8YCPLT"
+};
